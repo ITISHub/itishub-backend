16ebe081751b614a0be603432a4fec7c7cae8886
Azat Gilyazov <azatpscl@gmail.com>
2020-08-03 17:11:45 +0300

    Registered  models in admin panel
    Registered  models in admin panel

--- itishub/home/admin.py
+++ itishub/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(HomeCard)
+admin.site.register(Creator)
+admin.site.register(Lesson)
+admin.site.register(Course)
